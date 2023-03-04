b185663e04ae56f903288129087308c63d5b12fa
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-03-04 22:04:04 +0000

    Deploying to gh-pages from @ ellinarel/goit-js-hw-10@edc7de95abf761b60acd76e87958f3dbdb3576e0 🚀

---
+++ index.fe084b9c.js
@@ -0,0 +1,2 @@
+const t={inputEl:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryList:document.querySelector(".country-info")};t.inputEl.addEventListener("input",(function(e){e.preventDefault();t.inputEl.value.trim()}));
+//# sourceMappingURL=index.fe084b9c.js.map
