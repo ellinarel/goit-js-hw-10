a22c3a7d2895820b7e40ab23b659d4641e998f2e
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-03-05 22:42:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,31 +14,31 @@ const DEBOUNCE_DELAY = 300;
 refs.inputEl.addEventListener(`input`,debounce(onInput,DEBOUNCE_DELAY)) 
 
 function onInput(e) {
-    e.preventDefault()
-    const name = refs.inputEl.value.trim();
-    if (name === '') {
-        return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '');
-    }
-    fetchCountries(name)
+  e.preventDefault()
+  const name = refs.inputEl.value.trim();
+  if (name === '') {
+    return (refs.countryList.innerHTML = ''), (refs.countryInfo.innerHTML = '');
+  }
+   
+ fetchCountries(name)
        .then(countries => {
       refs.countryList.innerHTML = '';
       refs.countryInfo.innerHTML = '';
 
       if (countries.length === 1) {
-        refs.countryList.insertAdjacentHTML('beforestart');
-        refs.countryInfo.insertAdjacentHTML('beforestart');
+        refs.countryList.insertAdjacentHTML('beforestart',updateCountryList());
+        refs.countryInfo.insertAdjacentHTML('beforestart',updateCountryInfo());
       } else if (countries.length >= 10) {
         Notiflix.Notify.info(
     `Too many matches found. Please enter a more specific name.`
   );
       } else {
-        refs.countryList.insertAdjacentHTML('beforestart');
+        refs.countryList.insertAdjacentHTML('beforestart',updateCountryList());
       }
     })
         
-   .catch( Notiflix.Notify.failure(`Oops, there is no country with that name`))
+   .catch(Notiflix.Notify.failure(`Oops, there is no country with that name`))
 }
-
 
 
 
